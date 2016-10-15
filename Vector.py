8a911222f9a21b65fbf614414c8013879f297aa9
salmedina <salbed.medina@gmail.com>
2016-10-15 03:43:28 -0400

    Added parallel and orthogonality check, as well as is_zero function and isclose to deal with decimal comparisons

--- Vector.py
+++ Vector.py
@@ -4,6 +4,11 @@ from decimal import Decimal, getcontext
 
 getcontext().prec = 30
 
+def isclose(a, b, rel_tol=1e-03, abs_tol=1e-10):
+    rel_tol = Decimal(rel_tol)
+    abs_tol = Decimal(abs_tol)
+    return abs(a-b) <= max(rel_tol * max(abs(a), abs(b)), abs_tol)
+
 class Vector(object):
     def __init__(self, coordinates):
         try:
@@ -63,16 +68,21 @@ class Vector(object):
         return (sum([x*x for x in self.coordinates]))**(Decimal(0.5))
 
     def unit(self):
-        mag = abs(self)
-        if mag == 0:
-            return 0
-        return (Decimal(1.0)/mag) * self
+        _, u = self.normalize()
+        return u
 
     def dot(self, other):
         return self * other
 
-    def normalized(self):
-        return self.unit()
+    def normalize(self):
+        '''
+        :return:mag: magnitude of the vector
+        :return:unit: unit vector
+        '''
+        mag = abs(self)
+        if isclose(mag, 0):
+            return 0, self
+        return mag, (Decimal(1.0) / mag) * self
 
     def angle(self, other, deg=False):
         if type(other) is not Vector:
@@ -88,38 +98,59 @@ class Vector(object):
             return angle_rad * Decimal(180.0/pi)
         return angle_rad
 
+    def is_zero(self):
+        return isclose(abs(self), Decimal(0.0))
+
     def is_aligned(self, other):
         if type(other) is not Vector:
             raise Exception("A Vector is required to calculate the angle")
-        return ( self.angle(other) == 0 )
+        return isclose(self.angle(other), Decimal(0))
 
     def is_opposite(self, other):
         if type(other) is not Vector:
             raise Exception("A Vector is required to calculate the angle")
-        return ( self.angle(other) == pi)
+        return isclose( self.angle(other), Decimal(pi) )
+
+    def is_parallel(self, other):
+        '''By definition'''
+        return (isclose(abs(self), Decimal(0.0)) or
+                isclose(abs(other), Decimal(0.0)) or
+                self.is_opposite(other) or
+                self.is_aligned(other) )
 
     def is_orthogonal(self, other):
         if type(other) is not Vector:
             raise Exception("A Vector is required to calculate the angle")
-        return (self * other) == 0
+        return isclose(self * other, Decimal(0))
 
 
-v1 = Vector([7.887, 4.138])
-v2 = Vector([-8.802, 6.776])
+v1 = Vector([-7.579, -7.88])
+w1 = Vector([22.737, 23.64])
 
-v3 = Vector([-5.955, -4.904, -1.874])
-v4 = Vector([-4.496, -8.755, 7.103])
+v2 = Vector([-2.029, 9.97, 4.172])
+w2 = Vector([-9.231, -6.639, -7.245])
 
-v5 = Vector([3.183, -7.627])
-v6 = Vector([-2.668, 5.319])
+v3 = Vector([-2.328, -7.284,-1.214])
+w3 = Vector([-1.821,1.072,-2.94])
 
-v7 = Vector([7.35, 0.221, 5.188])
-v8 = Vector([2.751, 8.259, 3.985])
+v4 = Vector([2.118, 4.827])
+w4 = Vector([0,0])
 
-print v1*v2
-print v3*v4
 
-print v5.angle(v6)
-print v7.angle(v8, deg=True)
-
+print v1.angle(w1)
+print v1.is_parallel(w1)
+print v1.is_orthogonal(w1)
+print ''
+print v2.angle(w2)
+print v2.is_parallel(w2)
+print v2.is_orthogonal(w2)
+print ''
+print v3.angle(w3)
+print v3*w3
+print v3.is_parallel(w3)
+print v3.is_orthogonal(w3)
+print ''
+print v4.angle(w4)
+print v4.is_parallel(w4)
+print v4.is_orthogonal(w4)
 
